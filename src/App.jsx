41714be7b2f4c44f2afd5edaaa2055af239104dd
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-12-12 15:13:22 +0100

    add: global state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Header from './components/layout/header/Header'
 import SearchPage from './pages/searchpage/SearchPage'
 import Footer from './components/layout/footer/Footer'
+import GlobalContext from './context/GlobalContext'
+import axios from 'axios'
 import './App.css'
 
 export default function App() {
 
   return (
-    <>
+    <GlobalContext.Provider value={{}}>
       <Header />
       <SearchPage />
       <Footer />
-    </>
+    </GlobalContext.Provider>
   )
 }
-
-
