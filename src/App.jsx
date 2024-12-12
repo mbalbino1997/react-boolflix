fce76cc34fcffc1ebf3e9a547ce6c316e9061a89
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-12-12 15:50:08 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,28 @@ import Footer from './components/layout/footer/Footer'
 import GlobalContext from './context/GlobalContext'
 import axios from 'axios'
 import './App.css'
+import { useState } from 'react'
 
 export default function App() {
+  const [moviesArray, setMoviesArray] = useState([]);
+  const BASE_URI = "https://api.themoviedb.org/3/search/movie";
+  const API_KEY = "3ddebe04883de61a2570f2eb4a6c7c21";
+  function fetchData() {
+    axios.get(BASE_URI, {
+      params: {
+        api_key: API_KEY,
+        query: "harry potter",
+        language: "it"
+      },
+    }).then((res) => {
+      setMoviesArray(res.data);
+      console.log(res.data);
 
+    }).catch((err) => { console.error(err.message); })
+  }
+  fetchData();
   return (
-    <GlobalContext.Provider value={{}}>
+    <GlobalContext.Provider value={{ fetchData, moviesArray, setMoviesArray }}>
       <Header />
       <SearchPage />
       <Footer />
